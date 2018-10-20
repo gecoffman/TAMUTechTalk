bb46ef03ad1e6c6dedc98ffba4d6f5d375455c79
Grace Coffman <grace.e.coffman@gmail.com>
2018-10-20 13:33:39 -0500

    Adding json data and service for testing.'

--- forex/service.py
+++ forex/service.py
@@ -24,7 +24,7 @@ def returnCCYData( ccyPair ):
 		return 'Currency pair does not exist. Try again'   
 
 if __name__ == "__main__":
-    app.run( host='0.0.0.0', port=8080 )
+    app.run( host='0.0.0.0', port=8081 )
 
 
 
